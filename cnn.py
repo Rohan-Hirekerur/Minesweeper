b974a5cbc87de3a51d3a67efb63ff56039dba838
Rohan Hirekerur <rohanhirekerur@gmail.com>
2019-01-19 22:59:09 +0530

    AI added

--- cnn.py
+++ cnn.py
@@ -1,35 +1,7 @@
 import tensorflow as tf
 import numpy as np
+from collections import deque
 import random
-
-
-state_size = [200, 200, 1]      # Our input is a stack of 4 frames hence 84x84x4 (Width, height, channels)
-action_size = 100             # 3 possible actions: left, right, shoot
-learning_rate = 0.0002      # Alpha (aka learning rate)
-
-# TRAINING HYPER PARAMETERS
-total_episodes = 500        # Total episodes for training
-max_steps = 100              # Max possible steps in an episode
-batch_size = 64
-
-# Exploration parameters for epsilon greedy strategy
-explore_start = 1.0            # exploration probability at start
-explore_stop = 0.01            # minimum exploration probability
-decay_rate = 0.0001            # exponential decay rate for exploration prob
-
-# Q learning hyper parameters
-gamma = 0.95               # Discounting rate
-
-# MEMORY HYPER PARAMETERS
-pretrain_length = batch_size   # Number of experiences stored in the Memory when initialized for the first time
-memory_size = 1000000          # Number of experiences the Memory can keep
-
-# MODIFY THIS TO FALSE IF YOU JUST WANT TO SEE THE TRAINED AGENT
-training = True
-
-# TURN THIS TO TRUE IF YOU WANT TO RENDER THE ENVIRONMENT
-episode_render = False
-
 
 class Cnn:
     def __init__(self, state_size, action_size, learning_rate):
@@ -39,7 +11,7 @@ class Cnn:
 
         with tf.variable_scope('Cnn'):
             self.inputs = tf.placeholder(tf.float32, [None, *state_size])
-            self.actions = tf.placeholder(tf.float32, [None, action_size])
+            self.actions = tf.placeholder(tf.float32, [None])
 
             self.sample_op = tf.placeholder(tf.float32, [None])
 
@@ -73,7 +45,7 @@ class Cnn:
                                       kernel_initializer=tf.contrib.layers.xavier_initializer())
 
             self.output = tf.layers.dense(inputs=self.fc, kernel_initializer=tf.contrib.layers.xavier_initializer(),
-                                          units=3, activation=None)
+                                          units=100, activation=None)
 
             self.pred_op = tf.reduce_sum(tf.multiply(self.output, self.actions))
 
--- minesweeper_ai.py
+++ minesweeper_ai.py
@@ -1,8 +1,41 @@
+from collections import deque
 import pygame
 import pyglet
 import random
 import math
 import numpy as np
+import cnn
+import tensorflow as tf
+
+state_size = [200, 200, 3]
+action_size = 100
+learning_rate = 0.0002      # Alpha (aka learning rate)
+
+# TRAINING HYPER PARAMETERS
+total_episodes = 1500        # Total episodes for training
+max_steps = 100              # Max possible steps in an episode
+batch_size = 100
+
+# Exploration parameters for epsilon greedy strategy
+explore_start = 1.0            # exploration probability at start
+explore_stop = 0.01            # minimum exploration probability
+decay_rate = 0.0001            # exponential decay rate for exploration prob
+
+# Q learning hyper parameters
+gamma = 0.95               # Discounting rate
+
+# MEMORY HYPER PARAMETERS
+pretrain_length = batch_size   # Number of experiences stored in the Memory when initialized for the first time
+memory_size = 1000000          # Number of experiences the Memory can keep
+
+# MODIFY THIS TO FALSE IF YOU JUST WANT TO SEE THE TRAINED AGENT
+training = False
+testing = True
+
+
+possible_actions = []
+for i in range(0, 100):
+    possible_actions.append(i)
 
 # Initialize number of rows and columns in the game
 num_rows = 10
@@ -164,8 +197,9 @@ class Grid:
     # If the clicked square is blank, all its surrounding squares are also shown
     # The function is recursively called to check if the surrounding squares contain empty square
     def show(self, column, row):
+        if not self.squares[column][row].clicked:
+            self.num_clicked += 1
         self.squares[column][row].clicked = True
-        self.num_clicked += 1
         self.squares[column][row].flagged = False
         if self.squares[column][row].is_bomb:
             return True
@@ -222,37 +256,280 @@ class Grid:
         return True
 
 
-# Here we actually play the game
-def play():
+def predict_action(explore_start, explore_stop, decay_rate, decay_step, state, actions):
+    # EPSILON GREEDY STRATEGY
+    # Choose action a from state s using epsilon greedy.
+    # First we randomize a number
+    exp_exp_tradeoff = np.random.rand()
+
+    # Here we'll use an improved version of our epsilon greedy strategy used in Q-learning notebook
+    explore_probability = explore_stop + (explore_start - explore_stop) * np.exp(-decay_rate * decay_step)
+
+    if explore_probability > exp_exp_tradeoff:
+        # Make a random action (exploration)
+        action = random.choice(possible_actions)
+
+    else:
+        # Get action from Q-network (exploitation)
+        # Estimate the Qs values state
+        Qs = sess.run(nn.output, feed_dict={nn.inputs: state.reshape((1, *state.shape))})
+
+        # Take the biggest Q value (= the best action)
+        choice = np.argmax(Qs)
+        action = possible_actions[int(choice)]
+
+    return action, explore_probability
+
+
+tf.reset_default_graph()
+
+# Instantiate the nn
+nn = cnn.Cnn(state_size, action_size, learning_rate)
+
+
+class Memory:
+    def __init__(self, max_size):
+        self.buffer = deque(maxlen=max_size)
+
+    def add(self, experience):
+        self.buffer.append(experience)
+
+    def sample(self, batch_size):
+        buffer_size = len(self.buffer)
+        index = np.random.choice(np.arange(buffer_size),
+                                 size=batch_size,
+                                 replace=False)
+
+        return [self.buffer[i] for i in index]
+
+
+memory = Memory(max_size=memory_size)
+
+# Render the environment
+
+"""
+for i in range(total_episodes):
+    # If it's the first step
+    state = pygame.surfarray.array3d(screen)
+
     grid = Grid()
     done = False
     bombs_found = 0
     while not done:
+        reward = 0
         screen.fill(black)
+
+        action = random.choice(possible_actions)
+
         if bombs_found == grid.num_bombs:
             done = grid.validate()
 
         events = pygame.event.get()
-        for event in events:
-            if event.type == pygame.QUIT or grid.num_clicked == (grid.rows*grid.columns)-grid.num_bombs:
-                done = True
-            if event.type == pygame.MOUSEBUTTONUP:
-                pos = pygame.mouse.get_pos()
-                x = math.floor(pos[0] / multiplier)
-                y = math.floor(pos[1] / multiplier)
-                if event.button == 1:               # 1 --> Left click
-                    is_bomb = grid.show(x, y)
-                    if is_bomb:
-                        grid.show_all_bombs()
-                        done = True
-                if event.button == 3:               # 3 --> Right click
-                    bombs_found = grid.flag(x, y, bombs_found)
+        x = math.floor(action/10)
+        y = action%10
+        is_bomb = grid.show(x, y)
+        if is_bomb:
+            grid.show_all_bombs()
+            reward = -101
+            done = True
+            next_state = np.zeros(state.shape)
+            memory.add((state, action, reward, next_state, done))
+        else:
+            reward = 1
+        if grid.num_clicked == (grid.rows * grid.columns) - grid.num_bombs:
+            reward = 101
+            done = True
 
-        clock.tick()
         grid.display()
         pygame.display.flip()
+        next_state = pygame.surfarray.array3d(screen)
+        state = next_state
+        memory.add((state, action, reward, next_state, done))
+"""
+saver = tf.train.Saver()
 
+if training:
+    with tf.Session() as sess:
+        # Initialize the variables
+        sess.run(tf.global_variables_initializer())
 
-play()
+        # Initialize the decay rate (that will use to reduce epsilon)
+        decay_step = 0
+
+        for episode in range(total_episodes):
+            # Set step to 0
+            step = 0
+
+            # Initialize the rewards of the episode
+            episode_rewards = []
+
+            # Make a new episode and observe the first state
+            state = pygame.surfarray.array3d(screen)
+            grid = Grid()
+            done = False
+            bombs_found = 0
+            actions = []
+
+            while step < max_steps:
+                step += 1
+                # Increase decay_step
+                decay_step += 50
+
+                reward = 0
+                screen.fill(black)
+
+                while True:
+                    action, explore_probability = predict_action(explore_start, explore_stop, decay_rate, decay_step,
+                                                                 state, possible_actions)
+                    if action not in actions:
+                        break
+                    # reward -= 1
+                actions.append(action)
+
+                if bombs_found == grid.num_bombs:
+                    done = grid.validate()
+
+                events = pygame.event.get()
+                x = math.floor(action / 10)
+                y = action % 10
+                print (x, y)
+                is_bomb = grid.show(x, y)
+                if is_bomb:
+                    grid.show_all_bombs()
+                    reward -= 101
+                    step = max_steps
+                    next_state = np.zeros(state.shape, dtype=np.int)
+                    memory.add((state, action, reward, next_state, done))
+                else:
+                    reward += 2
+                if grid.num_clicked == (grid.rows * grid.columns) - grid.num_bombs:
+                    reward -= 101
+                    step = max_steps
+
+                episode_rewards.append(reward)
+                grid.display()
+                pygame.display.flip()
+                next_state = pygame.surfarray.array3d(screen)
+                memory.add((state, action, reward, next_state, done))
+                state = next_state
+
+                # LEARNING PART
+                # Obtain random mini-batch from memory
+                batch = memory.sample(batch_size)
+                states_mb = np.array([each[0] for each in batch], ndmin=3)
+                actions_mb = np.array([each[1] for each in batch])
+                rewards_mb = np.array([each[2] for each in batch])
+                next_states_mb = np.array([each[3] for each in batch], ndmin=3)
+                dones_mb = np.array([each[4] for each in batch])
+
+                target_Qs_batch = []
+
+                # Get Q values for next_state
+                Qs_next_state = sess.run(nn.output, feed_dict={nn.inputs: next_states_mb})
+
+                # Set Q_target = r if the episode ends at s+1, otherwise set Q_target = r + gamma*maxQ(s', a')
+                for i in range(0, len(batch)):
+                    terminal = dones_mb[i]
+
+                    # If we are in a terminal state, only equals reward
+                    if terminal:
+                        target_Qs_batch.append(rewards_mb[i])
+
+                    else:
+                        target = rewards_mb[i] + gamma * np.max(Qs_next_state[i])
+                        target_Qs_batch.append(target)
+
+                targets_mb = np.array([each for each in target_Qs_batch])
+
+                loss, _ = sess.run([nn.loss, nn.optimizer],
+                                   feed_dict={nn.inputs: states_mb,
+                                              nn.sample_op: targets_mb,
+                                              nn.actions: actions_mb})
+
+            # Save model every 5 episodes
+            if episode % 5 == 0:
+                save_path = saver.save(sess, "./model.ckpt")
+                print("Model Saved")
+
+# Here we actually play the game
+# def play():
+#     grid = Grid()
+#     done = False
+#     bombs_found = 0
+#     while not done:
+#         screen.fill(black)
+#         if bombs_found == grid.num_bombs:
+#             done = grid.validate()
+#
+#         events = pygame.event.get()
+#         for event in events:
+#             if event.type == pygame.QUIT or grid.num_clicked == (grid.rows*grid.columns)-grid.num_bombs:
+#                 done = True
+#             if event.type == pygame.MOUSEBUTTONUP:
+#                 pos = pygame.mouse.get_pos()
+#                 x = math.floor(pos[0] / multiplier)
+#                 y = math.floor(pos[1] / multiplier)
+#                 if event.button == 1:               # 1 --> Left click
+#                     is_bomb = grid.show(x, y)
+#                     if is_bomb:
+#                         grid.show_all_bombs()
+#                         done = True
+#                 if event.button == 3:               # 3 --> Right click
+#                     bombs_found = grid.flag(x, y, bombs_found)
+#
+#         clock.tick()
+#         grid.display()
+#         pygame.display.flip()
+#
+#
+# play()
+
+if testing:
+    with tf.Session() as sess:
+        # Initialize the variables
+        saver.restore(sess, "./model.ckpt")
+        sess.run(tf.global_variables_initializer())
+
+        for episode in range(1):
+            state = pygame.surfarray.array3d(screen)
+            grid = Grid()
+            done = False
+            bombs_found = 0
+            actions = []
+
+            while not done:
+                screen.fill(black)
+                Qs = sess.run(nn.output, feed_dict={nn.inputs: state.reshape((1, *state.shape))})
+
+                while True:
+                    choice = np.argmax(Qs)
+                    action = possible_actions[int(choice)]
+                    Qs[0][choice] = -1000
+                    if action not in actions:
+                        break
+                actions.append(action)
+
+                if bombs_found == grid.num_bombs:
+                    done = grid.validate()
+
+                events = pygame.event.get()
+                x = math.floor(action / 10)
+                y = action % 10
+                print(x, y)
+                is_bomb = grid.show(x, y)
+                if is_bomb:
+                    grid.show_all_bombs()
+                    done = True
+                    next_state = np.zeros(state.shape, dtype=np.int)
+
+                if grid.num_clicked == (grid.rows * grid.columns) - grid.num_bombs:
+                    done = True
+
+                pygame.time.delay(500)
+                grid.display()
+                pygame.display.flip()
+                next_state = pygame.surfarray.array3d(screen)
+                state = next_state
+
 pygame.time.delay(3000)
 pygame.quit()
